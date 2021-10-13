74f0260d6a60544bc82335f204480ba7abdb7102
Antoine LUDEAU <antoine.ludeau@3ds.com>
2021-10-13 20:35:00 +0200

    added first animations

--- src/components/organisms/first-screen.tsx
+++ src/components/organisms/first-screen.tsx
@@ -1,10 +1,28 @@
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import { useSpring, animated } from "react-spring";
 
 import HomeImage from "../../assets/image/home.jpg";
 
 export const FirstScreen = () => {
+  const props = useSpring({
+    to: { opacity: 1, x: 10 },
+    from: { opacity: 0, x: 0 },
+    config: { tension: 30 },
+  });
+  const props2 = useSpring({
+    to: { opacity: 1, x: 10 },
+    from: { opacity: 0, x: 0 },
+    config: { tension: 30 },
+    delay: 400
+  });
+  const props3 = useSpring({
+    to: { opacity: 1, x: 10 },
+    from: { opacity: 0, x: 0 },
+    config: { tension: 30 },
+    delay: 800
+  });
   return (
     <>
       <Box
@@ -18,25 +36,31 @@ export const FirstScreen = () => {
           sx={{
             position: "absolute",
             top: "50%",
-            transform: "translateY(-50%)"
+            transform: "translateY(-50%)",
           }}
           container
           spacing={2}
         >
           <Grid item xs={12}>
-            <Typography variant="h1" component="div">
-              Hello, I'm
-            </Typography>
+            <animated.div style={props}>
+              <Typography variant="h1" component="div">
+                Hello I'm
+              </Typography>
+            </animated.div>
           </Grid>
           <Grid item xs={12}>
-            <Typography variant="h2" component="div">
-              Antoine Ludeau
-            </Typography>
+            <animated.div style={props2}>
+              <Typography variant="h2" component="div">
+                Antoine Ludeau
+              </Typography>
+            </animated.div>
           </Grid>
           <Grid item xs={12}>
-            <Typography variant="h4" component="div">
-              Web Developer Full Stack
-            </Typography>
+            <animated.div style={props3}>
+              <Typography variant="h4" component="div">
+                Web Developer Full Stack
+              </Typography>
+            </animated.div>
           </Grid>
         </Grid>
       </Box>
