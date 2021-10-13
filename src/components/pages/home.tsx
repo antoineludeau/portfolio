f5589823d1d3399f8b8368708bf6e9b6b8102a9c
Antoine LUDEAU <antoine.ludeau@3ds.com>
2021-10-13 16:39:45 +0200

    added home template and page skeleton

---
+++ src/components/pages/home.tsx
@@ -0,0 +1,7 @@
+import {TemplateHome} from '../templates'
+
+export const Home = () => {
+  return (
+    <TemplateHome/>
+  )
+}
---
+++ src/components/templates/home.tsx
@@ -0,0 +1,7 @@
+
+
+export const TemplateHome = () => {
+  return (
+    <div/>
+  )
+}
