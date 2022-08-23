ca41a4f2d22cc5578ea0a2c92ab5b662e118ed4e
antoine ludeau <alu9@3ds.com>
2022-08-23 11:27:32 +0200

    Cleaned unused import

--- src/components/molecules/menu.tsx
+++ src/components/molecules/menu.tsx
@@ -6,7 +6,6 @@ import Slide from "@mui/material/Slide";
 import Drawer from "@mui/material/Drawer";
 import Box from "@mui/material/Box";
 import List from "@mui/material/List";
-import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
