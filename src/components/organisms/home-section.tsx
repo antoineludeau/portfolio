5a5418638de01150a48e380dc5110527e70f4101
Antoine LUDEAU <antoineludeau@MacBook-Air-de-Antoine.local>
2022-10-17 13:35:11 +0200

    Added contact features to home page

--- src/components/organisms/home-section.tsx
+++ src/components/organisms/home-section.tsx
@@ -1,12 +1,37 @@
+import { useState } from 'react'
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import { useSpring, animated } from "react-spring";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import IconButton from '@mui/material/IconButton';
+import Link from '@mui/material/Link';
+import DialogTitle from '@mui/material/DialogTitle';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import Button from '@mui/material/Button';
 
 import HomeImage from "../../assets/image/home.jpg";
+import Linkedin from '../../assets/image/linkedin.png'
+import Phone from '../../assets/image/phone.png'
+import Mail from '../../assets/image/mail.png'
+
+interface openContactDialogProps {
+  open: boolean,
+  title: string,
+  icon: string,
+  content: string
+}
+
+const defaultContactDialogState = {
+  open: false,
+  title: '',
+  icon: '',
+  content: ''
+}
 
 export const HomeSection = () => {
+  const [contactDialogState, setContactDialogState] = useState<openContactDialogProps>(defaultContactDialogState)
   const props = useSpring({
     to: { opacity: 1, x: 0 },
     from: { opacity: 0, x: -10 },
@@ -76,6 +101,22 @@ export const HomeSection = () => {
                 </Typography>
               </animated.div>
             </Grid>
+            <Grid item xs={12}>
+              <Box
+              >
+                <Link href="https://www.linkedin.com/in/antoineludeau/" target="_blank" color="inherit">
+                  <IconButton color="primary" aria-label="upload picture" component="label">
+                    <img width='30px' src={Linkedin} />
+                  </IconButton>
+                </Link>
+                <IconButton color="primary" aria-label="upload picture" component="label" onClick={() => setContactDialogState({ open: true, title: 'Téléphone', icon: Phone, content: '+33 6 37 69 25 19' })}>
+                  <img width='30px' src={Phone} />
+                </IconButton>
+                <IconButton color="primary" aria-label="upload picture" component="label" onClick={() => setContactDialogState({ open: true, title: 'Mail', icon: Mail, content: 'antoine.ludeau@gmail.com' })}>
+                  <img width='30px' src={Mail} />
+                </IconButton>
+              </Box>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
@@ -90,6 +131,36 @@ export const HomeSection = () => {
           <ArrowDownwardIcon fontSize="large" />
         </animated.div>
       </Box>
+      <Dialog onClose={() => setContactDialogState(prev => ({ ...prev, open: false }))} open={contactDialogState.open}>
+        <DialogTitle>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Box sx={{ display: 'inherit', alignItems: 'inherit', marginRight: 1 }}>
+              <img width='30px' height='30px' src={contactDialogState.icon} />
+            </Box>
+            <Typography variant="h4" component="div">
+              {contactDialogState.title}
+            </Typography>
+          </Box>
+        </DialogTitle>
+        <DialogContent>
+          <Grid
+            container
+            direction="column"
+            justifyContent="center"
+            alignItems="left"
+            padding={2}
+            spacing={4}
+          >
+            <Grid item>
+              <Typography variant="h5" component="div" >
+                {contactDialogState.content}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Button variant="outlined" onClick={() => navigator.clipboard.writeText(contactDialogState.content)}>Copier dans le clipboard</Button></Grid>
+          </Grid>
+        </DialogContent>
+      </Dialog>
     </>
   );
 };
