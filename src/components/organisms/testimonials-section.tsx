c36632b5a77966727e60d0babf316339ebe1387a
Antoine LUDEAU <antoineludeau@air-de-antoine.home>
2022-10-31 15:29:48 +0100

    Fixed number of testimonials

--- src/components/organisms/testimonials-section.tsx
+++ src/components/organisms/testimonials-section.tsx
@@ -18,20 +18,20 @@ import { SectionTitle, TestimonialCard } from "../molecules";
 
 const testimonials = [
   {
-    avatar: "",
-    title: "Jean-Pascal Torres",
-    subtitle: "Responsable R&D Plaform - 3DVIA - Dassault Systèmes",
-    subtitle2: "En 2022, Jean-Pascal était mon manager direct",
-    description:
-      "Toujours positif, motivé, impliqué et pertinent. Antoine a fait preuve d'excellentes capacités d'apprentissage et d'adaptation. Un plaisir de l'avoir eu comme collaborateur.",
-  },
-  {
     avatar: ThibautTavernier,
     title: "Thibaut Tavernier",
     subtitle: "Responsable Equipe Chef de Projet - 3DVIA - Dassault Systèmes",
     subtitle2: "En 2019, Thibaut était mon manager direct",
     description:
       "J'ai eu la chance de recruter Antoine en pleine construction d'une nouvelle équipe et j'ai pu m'appuyer sur son application, son sérieux et sa force de proposition et d'innovation pour mener avec lui les grands changements concernant le développement de nos applications de contribution de contenu. Antoine est appliqué, consciencieux et toujours souriant. Ça été un réel plaisir de travailler avec lui ces 5 dernières années.",
+  },
+  {
+    avatar: "",
+    title: "Jean-Pascal Torres",
+    subtitle: "Responsable R&D Plaform - 3DVIA - Dassault Systèmes",
+    subtitle2: "En 2022, Jean-Pascal était mon manager direct",
+    description:
+      "Toujours positif, motivé, impliqué et pertinent. Antoine a fait preuve d'excellentes capacités d'apprentissage et d'adaptation. Un plaisir de l'avoir eu comme collaborateur.",
   },
   {
     avatar: GauthierSaillard,
@@ -57,7 +57,7 @@ const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 export const TestimonialsSection = () => {
   const theme = useTheme();
   const [activeStep, setActiveStep] = useState(0);
-  const maxSteps = 3;
+  const maxSteps = 4;
 
   const handleNext = () => {
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
