6604e0232eac93fd3df17808799b44d34287a382
Antoine LUDEAU <antoineludeau@pc60.home>
2022-11-25 14:57:47 +0100

    Cleaned components

--- src/components/templates/main.tsx
+++ src/components/templates/main.tsx
@@ -28,34 +28,34 @@ export const TemplateMain = ({
   return (
     <>
       {menu}
-      <Grid container spacing={0}>
+      <Grid container>
         <Grid id="home" item xs={12}>
           {homeSection}
         </Grid>
-      </Grid>
-      <Grid id="presentation" item xs={12}>
-        {presentationSection}
-      </Grid>
-      <Grid id="philosophy" item xs={12}>
-        {philosophySection}
-      </Grid>
-      <Grid id="services" item xs={12}>
-        {servicesSection}
-      </Grid>
-      <Grid id="skills" item xs={12}>
-        {skillsSection}
-      </Grid>
-      <Grid id="jobs" item xs={12}>
-        {jobsSection}
-      </Grid>
-      <Grid id="testimonials" item xs={12}>
-        {testimonialsSection}
-      </Grid>
-      <Grid id="contacts" item xs={12}>
-        {contactsSection}
-      </Grid>
-      <Grid id="contacts" item xs={12}>
-        {footerSection}
+        <Grid id="presentation" item xs={12}>
+          {presentationSection}
+        </Grid>
+        <Grid id="philosophy" item xs={12}>
+          {philosophySection}
+        </Grid>
+        <Grid id="services" item xs={12}>
+          {servicesSection}
+        </Grid>
+        <Grid id="skills" item xs={12}>
+          {skillsSection}
+        </Grid>
+        <Grid id="jobs" item xs={12}>
+          {jobsSection}
+        </Grid>
+        <Grid id="testimonials" item xs={12}>
+          {testimonialsSection}
+        </Grid>
+        <Grid id="contacts" item xs={12}>
+          {contactsSection}
+        </Grid>
+        <Grid id="contacts" item xs={12}>
+          {footerSection}
+        </Grid>
       </Grid>
     </>
   );
