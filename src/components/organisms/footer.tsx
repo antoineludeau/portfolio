e9934cd8b150352e6cb9094dc7db2d728554ec30
Antoine LUDEAU <antoineludeau@MacBook-Air-de-Antoine.local>
2022-11-29 17:53:18 +0100

    Added code source link on footer

--- src/components/organisms/footer.tsx
+++ src/components/organisms/footer.tsx
@@ -1,19 +1,29 @@
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import InfoIcon from "@mui/icons-material/Info";
+import Link from "@mui/material/Link";
 
 export const FooterSection = () => {
   return (
-    <Grid container spacing={2} p={2}>
+    <Grid container p={2}>
       <Grid item xs={12}>
         <Typography
           textAlign="center"
           variant="body2"
           component="div"
           sx={{ fontStyle: "italic" }}
-          mb={2}
         >
-          Ce portfolio a été créé avec React, Typescript et Material-UI
+          Ce portfolio a été créé avec React, Typescript et Material-UI.
         </Typography>
+      </Grid>
+      <Grid item xs={12} textAlign="center">
+        <Link
+          href="https://github.com/antoineludeau/portfolio"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Code source
+        </Link>
       </Grid>
     </Grid>
   );
