08642a6abb758baf7e7cbf7e2fcf0b14d54cc117
Antoine LUDEAU <antoineludeau@MacBook-Air-de-Antoine.local>
2022-12-01 17:35:07 +0100

    Cleaned unused import

--- src/components/organisms/jobs-section.tsx
+++ src/components/organisms/jobs-section.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 
