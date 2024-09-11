8e6292c98e88a3951bdfad99d932d0c068110486
antoineludeau <52679050+antoineludeau@users.noreply.github.com>
2024-09-11 16:39:55 +0200

    Added missing tags to improve accessibility

--- components/molecules/ContactLinks.tsx
+++ components/molecules/ContactLinks.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 const ContactLinks = () => (
   <>
-    <Link href="https://www.linkedin.com/in/antoineludeau/" target="_blank">
-      <Image src="/linkedin.png" width={30} height={30} alt="Lien LinkedIn" />
+    <Link href="https://www.linkedin.com/in/antoineludeau/" target="_blank" aria-label='Lien LinkedIn'>
+      <Image src="/linkedin.png" width={30} height={30} alt="Logo LinkedIn" />
     </Link>
-    <Link href="https://github.com/antoineludeau" target="_blank">
-      <Image src="/github.png" width={30} height={30} alt="Lien Github" />
+    <Link href="https://github.com/antoineludeau" target="_blank" aria-label='Lien Github'>
+      <Image src="/github.png" width={30} height={30} alt="Logo Github" />
     </Link>
-    <Link href="https://www.malt.fr/profile/antoineludeau" target="_blank">
-      <Image src="/malt.png" width={30} height={30} alt="Lien Malt" />
+    <Link href="https://www.malt.fr/profile/antoineludeau" target="_blank" aria-label='Lien Malt'>
+      <Image src="/malt.png" width={30} height={30} alt="Logo Malt" />
     </Link>
   </>
 );
