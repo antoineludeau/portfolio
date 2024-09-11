e9c59e0bd0835cc0a56bf4323828f5e94ba4d87b
antoineludeau <52679050+antoineludeau@users.noreply.github.com>
2024-09-11 17:16:36 +0200

    Fixed tailwind config using extend option

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,17 +7,14 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    backgroundSize: {
-      'auto': 'auto',
-      'cover': 'cover',
-      'contain': 'contain',
-      '100%_20px': '100% 20px',
-    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      backgroundSize: {
+        '100%_20px': '100% 20px',
       },
       keyframes: {
         slidein: {
