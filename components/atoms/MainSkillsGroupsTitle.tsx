6583ff592d995b27881fbab6839f2ade4daa380f
antoineludeau <52679050+antoineludeau@users.noreply.github.com>
2024-09-11 17:15:58 +0200

    Modified color contrast to improve accessibility

--- components/atoms/MainSkillsGroupsTitle.tsx
+++ components/atoms/MainSkillsGroupsTitle.tsx
@@ -6,8 +6,8 @@ const MainSkillsGroupsTitle = ({
   iconComponent: React.ReactElement;
 }) => {
   return (
-    <div className="flex justify-center gap-2 mb-4">
-      <h3 className="text-red-400 text-gray-900 text-3xl">{title}</h3>
+    <div className="flex justify-center items-center gap-2 mb-4">
+      <h3 className="text-red-400 font-medium text-3xl">{title}</h3>
       <div className="stroke-red-400">{iconComponent}</div>
     </div>
   );
